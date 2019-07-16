8b299e6e54d136c8b7d21eaa7ce70ca6ff3f149b
shahid497 <52230472+shahid497@users.noreply.github.com>
2019-07-16 09:58:56 +0500

    login screen added

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+$error='';
+if(isset($_POST['submit'])){
+	if(empty($_POST['user']) || empty($_POST['pass']))
+	{
+		$error = "Username or Password is Empty";
+	}
+	else
+	{
+		$user=$_POST['user'];
+		$pass=$_POST['pass'];
+		$conn = mysqli_connect("localhost", "root", "");
+		$db = mysqli_select_db($conn, "crud");
+		$query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
+		$rows = mysqli_num_rows($query);
+		if($rows == 1){
+			header("Location: view.php");
+		}
+		else
+		{
+			$error = "Username or Password is Invalid";
+		}
+		mysqli_close($conn);
+	}
+}
+?>
+<!doctype html>
+<html>
+<head class="bg-dark">
+<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body class="bg-dark text-white">
+<div class="col-lg-6 m-auto"><br><br><br><br><br><br><br>
+		<form method="post" action="">
+			<div class="card">
+				<div class="card-header btn btn-outline-primary">
+					<h1 class="text-black text-center">
+						User Authentication
+					</h1>
+				</div><br>
+				<label>
+				</label>
+				<input type="text" placeholder="Enter username" name="user" class="form-control"><br>
+				<label>
+				</label>
+				<input type="text" placeholder="Enter password" name="pass" class="form-control"><br>
+				<button class="btn btn-outline-primary" type="submit" name="submit">Login</button>
+				<h3 class="text-center text-danger">
+					<span><?php echo $error; ?></span>
+				</h3>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
