ee55cacb9a794e816ad62586e41b11f4a42271ea
shahid497 <52230472+shahid497@users.noreply.github.com>
2019-07-19 00:11:54 +0500

    second login commit

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
+
 <?php
+session_start();
 $error='';
 if(isset($_POST['submit'])){
 	if(empty($_POST['user']) || empty($_POST['pass']))
@@ -8,12 +10,13 @@ if(isset($_POST['submit'])){
 	else
 	{
 		$user=$_POST['user'];
-		$pass=$_POST['pass'];
+		$pass=md5($_POST['pass']);
+		echo "$pass";
 		$conn = mysqli_connect("localhost", "root", "");
 		$db = mysqli_select_db($conn, "crud");
 		$query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
-		$rows = mysqli_num_rows($query);
-		if($rows == 1){
+		$_SESSION['rows'] = mysqli_num_rows($query);
+		if($_SESSION['rows'] == 1){
 			header("Location: view.php");
 		}
 		else
@@ -26,7 +29,7 @@ if(isset($_POST['submit'])){
 ?>
 <!doctype html>
 <html>
-<head class="bg-dark">
+<head>
 <title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -34,12 +37,12 @@ if(isset($_POST['submit'])){
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
-<body class="bg-dark text-white">
+<body>
 <div class="col-lg-6 m-auto"><br><br><br><br><br><br><br>
-		<form method="post" action="">
+		<form method="post">
 			<div class="card">
-				<div class="card-header btn btn-outline-primary">
-					<h1 class="text-black text-center">
+				<div class="card-header bg-dark">
+					<h1 class="text-white text-center">
 						User Authentication
 					</h1>
 				</div><br>
@@ -48,7 +51,7 @@ if(isset($_POST['submit'])){
 				<input type="text" placeholder="Enter username" name="user" class="form-control"><br>
 				<label>
 				</label>
-				<input type="text" placeholder="Enter password" name="pass" class="form-control"><br>
+				<input type="password" placeholder="Enter password" name="pass" class="form-control"><br>
 				<button class="btn btn-outline-primary" type="submit" name="submit">Login</button>
 				<h3 class="text-center text-danger">
 					<span><?php echo $error; ?></span>
